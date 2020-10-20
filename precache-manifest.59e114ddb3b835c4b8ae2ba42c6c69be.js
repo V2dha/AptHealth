5f267747a16c22483ab4f28d63d9281b803deb6b
V2dha <rvividha@gmail.com>
2020-10-20 20:49:59 +0530

    Updates

---
+++ precache-manifest.59e114ddb3b835c4b8ae2ba42c6c69be.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d0611342b4ec19786dbc4d7d425d88",
+    "url": "/whyiamawesome/index.html"
+  },
+  {
+    "revision": "75b58409e0fd543f1207",
+    "url": "/whyiamawesome/static/css/3.70d62d19.chunk.css"
+  },
+  {
+    "revision": "0d5fa885f14c0dbeda8d",
+    "url": "/whyiamawesome/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "5113981443e1a5a2a3a4",
+    "url": "/whyiamawesome/static/css/main.65887e46.chunk.css"
+  },
+  {
+    "revision": "162d3c16ce2d0450a767",
+    "url": "/whyiamawesome/static/js/2.a166f359.chunk.js"
+  },
+  {
+    "revision": "75b58409e0fd543f1207",
+    "url": "/whyiamawesome/static/js/3.38e69b67.chunk.js"
+  },
+  {
+    "revision": "0d5fa885f14c0dbeda8d",
+    "url": "/whyiamawesome/static/js/4.24c0b227.chunk.js"
+  },
+  {
+    "revision": "5113981443e1a5a2a3a4",
+    "url": "/whyiamawesome/static/js/main.f1ca56dd.chunk.js"
+  },
+  {
+    "revision": "c3c160a6ad058ff54a3f",
+    "url": "/whyiamawesome/static/js/runtime-main.4f95d77d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/whyiamawesome/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/whyiamawesome/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f127074c508c16a627971203dec72f4e",
+    "url": "/whyiamawesome/static/media/NOvid-20.f127074c.PNG"
+  },
+  {
+    "revision": "af6b8c85aababf99aaca3f7fb980d4a4",
+    "url": "/whyiamawesome/static/media/acm.af6b8c85.jpg"
+  },
+  {
+    "revision": "ec05628f9ff80b53cc02acd7e165f126",
+    "url": "/whyiamawesome/static/media/analyticvidhyaLogo.ec05628f.png"
+  },
+  {
+    "revision": "3f4fd680434844303ed21f1fd6c8da6e",
+    "url": "/whyiamawesome/static/media/asrd.3f4fd680.webp"
+  },
+  {
+    "revision": "7b19c3f936783e0fb97448935e5a7e27",
+    "url": "/whyiamawesome/static/media/careerlauncherLogo.7b19c3f9.png"
+  },
+  {
+    "revision": "f1e77359136002069687488bd99ffbe6",
+    "url": "/whyiamawesome/static/media/codecnetworksLogo.f1e77359.png"
+  },
+  {
+    "revision": "d562b9da8fe80ab1b891862c9c5cc1bc",
+    "url": "/whyiamawesome/static/media/codergirl.d562b9da.svg"
+  },
+  {
+    "revision": "4789d3f9871bb15964c8506fe2971884",
+    "url": "/whyiamawesome/static/media/codingninjasLogo.4789d3f9.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/whyiamawesome/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/whyiamawesome/static/media/deeplearningLogo.7a9b2eb0.png"
+  },
+  {
+    "revision": "592d5bcaf64567301a80f0072f343919",
+    "url": "/whyiamawesome/static/media/esummit.592d5bca.jpg"
+  },
+  {
+    "revision": "940cd8c49727ec75414e708cf3477e23",
+    "url": "/whyiamawesome/static/media/gbu.940cd8c4.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/whyiamawesome/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "808df7cfddca1aa21005e3fb3067311e",
+    "url": "/whyiamawesome/static/media/mlh.808df7cf.jpeg"
+  },
+  {
+    "revision": "22763c27761646948145310977dc5577",
+    "url": "/whyiamawesome/static/media/msa.22763c27.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/whyiamawesome/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "f72319f55ec3f40868a66aaadfdb3c7d",
+    "url": "/whyiamawesome/static/media/proficiency.f72319f5.svg"
+  },
+  {
+    "revision": "7d5ecf3e106b5672967528c76dc7a638",
+    "url": "/whyiamawesome/static/media/reading.7d5ecf3e.svg"
+  }
+]);
